67889a5a396115c4ae540011d07b27b975dbee13
Hesaam Farhang <hesaam@2600hz.com>
2018-07-17 02:17:01 -0700

    creating a base docker image for kazoo dev

---
+++ kazoo/images/kz-dev-base/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+OTP_VERSION=${OTP_VERSION:-21}
+
+docker build . --build-arg ERL_VERSION=${OTP_VERSION} -t kz-dev-base:${OTP_VERSION}
--- kazoo/images/kz-dev-base/entrypoint.sh
+++ kazoo/images/kz-dev-base/entrypoint.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
-
+set -o xtrace
 # Add local user
 # Either use the LOCAL_USER_ID if passed in at runtime or fallback to 1000
 
 USER_ID=${LOCAL_USER_ID:-1000}
 
-userdel -r kazoo
-
 echo "Starting with UID : $USER_ID"
-useradd --shell /bin/bash -u $USER_ID -o -c "" -m kazoo
+useradd --shell /bin/bash -u $USER_ID -o -M kazoo
+mkdir -p /home/kazoo
+cp -R /etc/skel/* /etc/skel/.[^.]* /home/kazoo
+chown -R kazoo:kazoo /home/kazoo
 export HOME=/home/kazoo
-mkdir $HOME/app
-
-export GID=`grep '^kazoo' /etc/group | cut -d':' -f 1`
-export UID=USER_ID
 
 exec /usr/bin/su-exec kazoo "$@"
