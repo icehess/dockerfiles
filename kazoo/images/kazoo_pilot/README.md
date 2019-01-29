02c2564cd4cd0a9e2b7c92a667b634fc76401f6c
Hesaam Farhang <icehess@gmail.com>
2019-01-28 19:13:56 -0800

    kazoo pilot

---
+++ kazoo/images/kazoo_pilot/README.md
@@ -0,0 +1,3 @@
+### erlang dependency:
+
+>    `libgpm2 libltdl7 libncurses5 libodbc1 libprocps6 libsctp1 libssl1.0.2 procps psmisc`
---
+++ kazoo/images/kazoo_pilot/kz_pilot/kzpilot/__init__.py
@@ -0,0 +1,48 @@
+import sys
+import json
+import docker
+
+from magiclog import log
+
+handle_types = ['container']
+stop_status = ['stop']
+container_status = ['start']
+
+log_events = ['config',
+              'container',
+              'network',
+              'secret',
+              'service',
+              'volume']
+
+
+class KzPilot:
+    def __init__(self,
+                 url='unix:///var/run/docker.sock'):
+        self.url = url
+        self.data = dict()
+
+    def start(self):
+        self.client = self.clientConnect()
+        for event in self.client.events(decode=True):
+            event_type = get_event_type(event)
+            if event_type in log_events:
+                self.logEvent(event, event_type)
+            log.debug('Event: {}'.format(json.dumps(event, indent=4, sort_keys=True)))
+
+    def clientConnect(self):
+        return docker.DockerClient(base_url=self.url)
+
+    def logEvent(self, event, event_type):
+        action = event['Action']
+
+
+    def shutdown(self):
+        if self.client is not None:
+            self.client.close()
+
+def get_event_type(event):
+    event['Type']
+
+def get_event_time(event):
+    event['time']
