42540765d016f3f7e4599abcc438dbc4344b1b2a
Hesaam Farhang <icehess@gmail.com>
2019-05-15 14:39:40 -0700

    fix sudo

--- kz-official-dev/images/kazoo-dev-env/docker-entrypoint.sh
+++ kz-official-dev/images/kazoo-dev-env/docker-entrypoint.sh
@@ -5,7 +5,7 @@ export LOCAL_USERID=${IOCAL_USERID:-1000}
 USE_LOCAL_USER=${USE_CI_USER:-true}
 
 if [ x"$USE_LOCAL_USER" = x"true" ]; then
-    (id -u $LOCAL_USERNAME >/dev/null 2>&1) || useradd -s /bin/bash -u $LOCAL_USERID -o -G wheel -m $LOCAL_USERNAME
+    (id -u $LOCAL_USERNAME >/dev/null 2>&1) || useradd -s /bin/bash -u $LOCAL_USERID -o -G sudo -m $LOCAL_USERNAME
     echo "Starting with UID : $LOCAL_USERID"
 
     export HOME=/home/$LOCAL_USERNAME
