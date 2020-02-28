8b675e072014d9e0f184438c189544a51a085007
Hesaam Farhang <icehess@gmail.com>
2020-02-28 15:41:49 -0800

    syncing

--- kazoo/images/kazoo-sounds/build.sh
+++ kazoo/images/kazoo-sounds/build.sh
@@ -4,6 +4,6 @@ TAG=${TAG:-latest}
 TAG=${KZ_SOUNDS_TAG:-$TAG}
 
 REPO=${REPO:-icehess}
-NAME=${KZ_SOUNDS_IMG_NAME:-kz-sounds}
+NAME=${KZ_SOUNDS_IMG_NAME:-kazoo-sounds}
 
 docker build . --rm --force-rm -t $REPO/$NAME:$TAG
