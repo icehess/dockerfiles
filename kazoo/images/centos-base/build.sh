536527cb162b05cb3cd252a9971561d27819ffa5
Hesaam Farhang <icehess@gmail.com>
2020-05-19 20:46:44 -0700

    wip centos-base

---
+++ kazoo/images/centos-base/build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+TAG=${TAG:-stable}
+TAG=${CENTOS_DEV_TAG:-$TAG}
+
+REPO=${REPO:-icehess}
+NAME=${CENTOS_DEV_IMG_NAME:-kz-centos-base}
+
+docker build --rm --force-rm . -t $REPO/$NAME:$TAG
