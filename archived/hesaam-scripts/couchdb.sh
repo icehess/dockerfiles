c0e07351bdd9d84e13d6a00da88ec4c774db976a
Hesaam Farhang <icehess@gmail.com>
2022-12-26 20:51:11 -0800

    refactoring, and cleanup

--- archived/hesaam-scripts/couchdb.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-curl -iX PUT 'http://127.0.0.1:5984/_global_changes'
-curl -iX PUT 'http://127.0.0.1:5984/_replicator'
-curl -iX PUT 'http://127.0.0.1:5984/_metadata'
-curl -iX PUT 'http://127.0.0.1:5984/_users'
--- kazoo/composer/kz-all-master-synchro/kz-rabbitmq-entry.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-rabbitmq-plugins enable --offline rabbitmq_consistent_hash_exchange
-/usr/local/bin/docker-entrypoint.sh "$@"
